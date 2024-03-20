99efce03e2d37f31b0bb19785b17098f268c3ac9
NASCIMENTOPC\nasci <nascimento.hate@gmail.com>
2024-03-20 12:20:56 -0300

    Mudando os dialetos para o MYSql e a inclusão do banco dados e criação da base de dados.

--- cursomc/src/main/java/com/luannascimento/cursomc/domain/Categoria.java
+++ cursomc/src/main/java/com/luannascimento/cursomc/domain/Categoria.java
@@ -2,6 +2,10 @@ package com.luannascimento.cursomc.domain;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -11,12 +15,15 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode
 @AllArgsConstructor
-
-
+@Entity
 public class Categoria implements Serializable{
+	
 		private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	
 		
