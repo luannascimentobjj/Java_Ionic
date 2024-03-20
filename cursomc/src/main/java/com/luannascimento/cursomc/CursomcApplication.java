8903d2042f17166f1eb958c648260c108e67fc21
NASCIMENTOPC\nasci <nascimento.hate@gmail.com>
2024-03-20 14:37:12 -0300

    Salvando arquivos, criando instância.

--- cursomc/src/main/java/com/luannascimento/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/luannascimento/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.luannascimento.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.luannascimento.cursomc.domain.Categoria;
+import com.luannascimento.cursomc.repositories.CategoriaRepository;
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Array;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepositoy;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Banco de Dados");
+		Categoria cat2 = new Categoria(null, "Java");
+		
+		categoriaRepositoy.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
