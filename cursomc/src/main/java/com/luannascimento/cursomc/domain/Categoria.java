83e105b6c803e8a4aeef2f3800b7dd17f3487035
NASCIMENTOPC\nasci <nascimento.hate@gmail.com>
2024-03-20 07:18:26 -0300

    Removendo anotações não necessárias para a classe domínio no Lombok.

--- cursomc/src/main/java/com/luannascimento/cursomc/domain/Categoria.java
+++ cursomc/src/main/java/com/luannascimento/cursomc/domain/Categoria.java
@@ -5,16 +5,13 @@ import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 @EqualsAndHashCode
+@AllArgsConstructor
+
 
 public class Categoria implements Serializable{
 		private static final long serialVersionUID = 1L;
